297ef1df3671acfa5890f9ee8386850d205066f0
kaorun <kaorun55@gmail.com>
2010-08-12 20:41:32 +0900

    PIN取得後のシグニチャ作成のテストを追加

--- OAuthTest/OAuthTest.cs
+++ OAuthTest/OAuthTest.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OAuth;
 
 namespace OAuthTest
 {
@@ -12,6 +13,18 @@ namespace OAuthTest
     [TestClass]
     public class OAuthTest
     {
+        // アクセストークン
+        public const string AccessToken = "https://cacoo.com/oauth/access_token";
+        public const string Authorize = "https://cacoo.com/oauth/authorize";
+        public const string ReqestToken = "https://cacoo.com/oauth/request_token";
+
+        // 通常は公開しないようにする
+        public const string ConsumerKey = "OKWyYVPvnpcBfbdmrJaNWx";
+        public const string ConsumerSecret = "wObshonqbOqrnhIXGwflmlEVUFiiEJqScYLmKbPYTe";
+
+        public const string Token = "17fb65da01aa217b9d11ab55d07e6f9b";
+        public const string TokenSecret = "933fee29fd7a2215d5bc40568e42f426";
+
         public OAuthTest()
         {
             //
@@ -60,11 +73,20 @@ namespace OAuthTest
         #endregion
 
         [TestMethod]
-        public void TestMethod1()
+        public void PINを取得後の認証()
         {
-            //
-            // TODO: テスト ロジックをここに追加してください
-            //
+            OAuth.OAuthBase oauth = new OAuth.OAuthBase();
+
+            Uri uri = new Uri( AccessToken );
+            string timestamp = "1281594194";
+            string nonce = "-4553643980892898909";
+            string pin = "8836708";
+            string normalizedUrl, normalizedRequestParameters, s;
+            string signature = oauth.GenerateSignature( uri, ConsumerKey, ConsumerSecret, Token, TokenSecret, "POST",
+                    timestamp, nonce, OAuthBase.SignatureTypes.HMACSHA1, pin,
+                    out normalizedUrl, out normalizedRequestParameters, out s );
+
+            Assert.AreEqual( "XIhBEU281BfkWcvgU20fKlrYCIk=", signature );
         }
     }
 }
